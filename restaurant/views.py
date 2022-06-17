e464b26abefe5a757126745fb8fc9d514235eaf3
valerieoni <valeriewil@yahoo.com>
2022-06-17 14:31:21 +0000

    add tests and an empty contact page

--- restaurant/views.py
+++ restaurant/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     """displays home page of the restaurant's website"""
     return render(request, 'restaurant/index.html')
+
+def contact(request):
+    """ displays contact us page"""
+    return render(request, 'restaurant/contact.html')
