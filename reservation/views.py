78f613796fcf5a6467254a0e9c52249b6e1b306f
valerieoni <valeriewil@yahoo.com>
2022-06-28 00:26:49 +0000

    add post method to save customer reservation request

--- reservation/views.py
+++ reservation/views.py
@@ -1,18 +1,48 @@
-from django.shortcuts import render
-from django.views import generic, View
-from .models import Reservation
+from django.shortcuts import render, reverse
+from django.views import View
+from django.http import HttpResponseRedirect
 from .forms import ReservationForm, CustomerForm
 
 
 class ReservationRequest(View):
     ''' returns reservation form '''
+    template_name = 'reservation/reservation_request.html'
 
     def get(self, request, *args, **kwargs):
+        """
+        display request form which comprises of 
+        the customer form and the reservation form
+        """
         reservation_form = ReservationForm()
         customer_form = CustomerForm()
-        template_name = 'reservation/reservation_request.html'
 
-        return render(request, template_name, {
+        return render(request, self.template_name, {
             'reservation_form': reservation_form,
             'customer_form': customer_form
+        })
+
+    def post(self, request, *args, **kwargs):
+        """
+        validate customer and reservation form.
+        check that table can be reserved.
+        If reservation can be made, then: 
+        - save request
+        - send email to restaurant recipeint so they know there's a new request
+        - display feedback message to customer
+        """
+        customer_form = CustomerForm(request.POST)
+        reservation_form = ReservationForm(request.POST)
+
+        if customer_form.is_valid() and reservation_form.is_valid():
+            customer = customer_form.save()
+            reservation = reservation_form.save(commit=False)
+            reservation.customer = customer
+            reservation.save()
+
+            return HttpResponseRedirect(reverse(
+                'reservation_request'))
+        else:
+            return render(request, self.template_name, {
+                'reservation_form': reservation_form,
+                'customer_form': customer_form
             })
