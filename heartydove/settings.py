897d0441f6fca13ff2b226b5f57c4ab06b081ce5
valerieoni <valeriewil@yahoo.com>
2022-10-09 08:43:30 +0000

    set debug to false

--- heartydove/settings.py
+++ heartydove/settings.py
@@ -30,10 +30,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get("APP_ENV") == "development":
-    DEBUG = True
-else:
-    DEBUG = False
+# if os.environ.get("APP_ENV") == "development":
+#    DEBUG = True
+# else:
+
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
